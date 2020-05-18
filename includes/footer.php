c13acca6c81efc786ae337771f01f2d36b70595c
Edward Gilbert <egbot@asu.edu>
2020-05-18 11:30:24 -0700

    Usage Policy remapping
    - Remapping usage policy to new location within /includes directory

--- includes/footer.php
+++ includes/footer.php
@@ -8,10 +8,9 @@
 		<td colspan="3" align="center" style="padding: 10px;border-top:solid black 1px;">
 	        <!-- this is the bottom section -->
 			<!-- <img src="<?php echo $clientRoot; ?>/images/layout/footer.gif" border="0"/> -->
-			Use of any specimen data and related material (e.g., images, species checklists, etc.) accessed through this portal requires agreement to the terms and conditions in the <a style="color:#816E68;font-weight:bold;" href="/portal/misc/usagepolicy.php">CNH data usage policy</a>.
+			Use of any specimen data and related material (e.g., images, species checklists, etc.) accessed through this portal requires agreement to the terms and conditions in the <a style="color:#816E68;font-weight:bold;" href="/portal/includes/usagepolicy.php">CNH data usage policy</a>.
 			<div>
 			</div>
 		</td>
 	</tr>
 </table>
-
