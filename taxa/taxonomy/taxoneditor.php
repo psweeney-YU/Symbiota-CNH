053f796fffb88b1fef4e918cec57b38bdc738e03
Nikita Salikov <nsalikov@asu.edu>
2025-10-17 08:14:56 -0700

    simplified sanitation, removed double encoding

--- taxa/taxonomy/taxoneditor.php
+++ taxa/taxonomy/taxoneditor.php
@@ -332,14 +332,11 @@ if ($isEditor) {
 							<div class="editfield">
 								<?php
 								$safeSource = $taxonEditorObj->getSource() ?? '';
-								if (stripos($safeSource, '<a ') === false) {
-									$safeSource = htmlspecialchars($safeSource);
-								}
-								echo $safeSource;
+								$safeSource = strip_tags($safeSource, '<a>');
 								?>
 							</div>
 							<div class="editfield" style="display:none;width:90%;">
-								<input type="text" id="source" name="source" style="width:100%;" value="<?php echo htmlspecialchars($safeSource); ?>" />
+								<input type="text" id="source" name="source" style="width:100%;" value="<?php echo $safeSource ?>" />
 							</div>
 						</div>
 						<div class="editDiv">
