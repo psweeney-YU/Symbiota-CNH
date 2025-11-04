921097f582392ed7f1f376c7df6202e42f1db9ab
Greg Post <gregory.post@ku.edu>
2025-11-04 11:52:38 -0600

    Handle empty client root on redacted portals (#3014)
    Fixes redacted portals that do not have a client root value. i.e. are on the root of the domain.

--- config/symbbase.php
+++ config/symbbase.php
@@ -62,7 +62,10 @@ $PORTAL_PRIVATE = $PRIVATE_VIEWING_ONLY ?? false;
 if (!$SYMB_UID && $PORTAL_PRIVATE){
 	$PRIVATE_VIEWING_OVERRIDES = $PRIVATE_VIEWING_OVERRIDES ?? [];
 	$public_pages = [...$PRIVATE_VIEWING_OVERRIDES, ...['/profile/newprofile.php', '/profile/index.php']];
-	$requested_url = explode($CLIENT_ROOT, $_SERVER['PHP_SELF'])[1];
+	if(!empty($CLIENT_ROOT)){
+		$requested_url = explode($CLIENT_ROOT, $_SERVER['PHP_SELF'])[1];
+	}
+	else $requested_url = $_SERVER['PHP_SELF'];
 	if (!in_array($requested_url, $public_pages)){
 		$referringUrl =  $_SERVER['PHP_SELF'] . (!empty($_SERVER['QUERY_STRING']) ? urlencode( '?' . $_SERVER['QUERY_STRING']) : '');
 		header('Location: ' . $CLIENT_ROOT . '/profile/index.php?refurl=' . $referringUrl);
