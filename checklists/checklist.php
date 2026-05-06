7038c954b05109a576d377e53dd4d50cfe656ba3
Edward Gilbert <egbot@asu.edu>
2026-05-06 13:09:30 -0400

    Hotfix checklist issues (#3533)
    - Voucher count now includes those from child checklists
    - Checklist parser changes
    -- Parsing taxon no longer required, thus improving copy/transfer functionality
    -- Update wording to include ability to use as a copy/transfer tool
    -- Improve Taxon ID (tid) updating and error handling when a taxon is selected, or not. e.g. updates tid when taxon changed or removed, block form submission if taxon is no in thesaurus, etc
    -- Allow copy and transfer to child checklists
    -- Add function that copies vouchers whenever "copy" is selected as the transfer method
    -- Ensure that current user is allows added as a manager, even when copy permission is not select, thus avoiding the creation of orphaned checklists
    
    Resolves issue https://github.com/Symbiota/Symbiota/issues/3524

--- checklists/checklist.php
+++ checklists/checklist.php
@@ -302,7 +302,7 @@ $taxonFilter = htmlspecialchars($taxonFilter, ENT_COMPAT | ENT_HTML401 | ENT_SUB
 				<?php
 			}
 			?>
-			<div style="clear:both">
+			<div style="clear:both; padding-top: 3px">
 				<hr/>
 			</div>
 			<div id="checklist-container" style="display:flex; gap: 0.5rem">
