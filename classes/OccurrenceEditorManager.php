12af2d10055aeb25f888f0e3f86bbeeb728a3f4b
Edward Gilbert <egbot@asu.edu>
2026-06-19 12:28:08 -0400

    Hotfix api bugs (#3604)
    * Initial commit
    
    * Sciname bug fix
    
    * Enforce timeout on all ajax calls
    
    * Add API option to only output statistics
    
    * Update portalindex.php
    
    - Rework Collection Statistics function to use new API endpoint
    
    * Bug fixes and add null search for tid
    
    * Update Swagger and other adjustments
    
    * Minor adjustment

--- classes/OccurrenceEditorManager.php
+++ classes/OccurrenceEditorManager.php
@@ -1,9 +1,9 @@
 <?php
 include_once($SERVER_ROOT . '/config/dbconnection.php');
 include_once($SERVER_ROOT . '/classes/OccurrenceDuplicate.php');
+include_once($SERVER_ROOT . '/classes/Media.php');
 include_once($SERVER_ROOT . '/classes/utilities/UuidFactory.php');
 include_once($SERVER_ROOT . '/classes/utilities/QueryUtil.php');
-include_once($SERVER_ROOT . '/classes/Media.php');
 include_once($SERVER_ROOT . '/classes/utilities/Language.php');
 
 Language::load('collections/editor/occurrenceeditor');
@@ -2706,6 +2706,66 @@ class OccurrenceEditorManager {
 			}
 		}
 		return $isEditor;
+	}
+
+	//Portal Index functions
+	public function triggerRemoteUpdates(){
+		if($this->occid && $GLOBALS['PORTAL_GUID'] && $GLOBALS['SECURITY_KEY']){
+			//Get list of remote occurrences and trigger API reharvesting events
+			$apiCallArr = array();
+			$sql = 'SELECT i.urlRoot, o.remoteOccid, o.portalOccurrencesID
+				FROM portalindex i INNER JOIN portalpublications p ON i.portalID = p.portalID
+				INNER JOIN portaloccurrences o ON p.pubid = o.pubid
+				WHERE p.direction = "export" AND i.securityKey IS NOT NULL AND o.occid = ?';
+			if($stmt = $this->conn->prepare($sql)){
+				$stmt->bind_param('i', $this->occid);
+				$stmt->execute();
+				$rs = $stmt->get_result();
+				while($r = $rs->fetch_object()){
+					$apiCallArr[$r->portalOccurrencesID] = $r->urlRoot . '/api/v2/installation/' . $GLOBALS['PORTAL_GUID'] .
+						'?eventCode=triggerHarvest&securityKey=' . $GLOBALS['SECURITY_KEY'] . '&remoteIdentifier=' . $r->remoteOccid;
+				}
+				$rs->free();
+				$stmt->close();
+			}
+			$this->processApiCalls($apiCallArr);
+		}
+	}
+
+	private function processApiCalls($apiCallArr){
+		//Uses the cURL Multi option to asynchronously trigger remote reharvest events
+		$channelArr = array();
+		$mh = curl_multi_init();
+		foreach ($apiCallArr as $key => $callArr) {
+			$url = $callArr['url'];
+			$channelArr[$key] = curl_init();
+
+			curl_setopt($channelArr[$key], CURLOPT_URL, $url);
+			curl_setopt($channelArr[$key], CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($channelArr[$key], CURLOPT_TIMEOUT, 10);
+
+			curl_multi_add_handle($mh, $channelArr[$key]);
+		}
+
+		// Execute requests simultaneously
+		$active = null;
+		do {
+			$status = curl_multi_exec($mh, $active);
+
+			if ($active) {
+				// Pause briefly to avoid maxing out your server's CPU
+				curl_multi_select($mh);
+			}
+		} while ($active && $status == CURLM_OK);
+
+		// Collect responses and clean up memory
+		foreach ($channelArr as $key => $ch) {
+			// Remove handle from manager and close
+			curl_multi_remove_handle($mh, $ch);
+			curl_close($ch);
+		}
+
+		curl_multi_close($mh);
 	}
 
 	//Misc data support functions
